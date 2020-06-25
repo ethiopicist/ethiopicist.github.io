fa0248c9c145e4c74c26a03687d9bd8ac25f46eb
Augustine Dickinson <shawn.m.dickinson@me.com>
2020-06-25 13:10:43 -0400

    Updates

--- assets/scripts/scripts.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-  $('#policy').click(function(){
-    $('.modal').addClass('is-active');
-  });
-
-  $('.modal-background, .modal-close').click(function(){
-    $('.modal').removeClass('is-active');
-  });
-});
